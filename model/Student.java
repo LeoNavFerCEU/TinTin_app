8a2ad00307dc89cc44fd8de0edd95314bcc2bb65
Sanyo2704 <alvarotormento2704@gmail.com>
2025-02-12 20:28:48 +0100

    model actualizado

--- model/Student.java
+++ model/Student.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package model;
 
 import lombok.Data;
@@ -13,19 +12,3 @@ public class Student {
 	private SchoolTutor tutor;
 	private Company company;
 }
-=======
-package model;
-
-import lombok.Data;
-
-@Data
-public class Student {
-	private Long id;
-	private String fullName;
-	private String course;
-	private String internshipPeriod;
-	private String courseYear;
-	private SchoolTutor tutor;
-	private Company company;
-}
->>>>>>> d7b63e85023e114d60811994d83fd37eb6ace028
--- model/User.java
+++ model/User.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package model;
 
 import lombok.Data;
@@ -12,18 +11,3 @@ public class User {
 	private Student associatedProfile;
 	private Boolean active;
 }
-=======
-package model;
-
-import lombok.Data;
-
-@Data
-public class User {
-	private Long id;
-	private String username;
-	private String password;
-	private String profileType;
-	private Student associatedProfile;
-	private Boolean active;
-}
->>>>>>> d7b63e85023e114d60811994d83fd37eb6ace028
