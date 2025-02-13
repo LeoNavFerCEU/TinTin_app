01ace45d96aa333aae02b418bf97ea9d53cbe2d3
Sanyo2704 <alvarotormento2704@gmail.com>
2025-02-13 17:21:53 +0100

    StudentService

--- TinTin_Project/src/main/java/tintin/services/StudentService.java
+++ TinTin_Project/src/main/java/tintin/services/StudentService.java
@@ -1,5 +1,12 @@
 package tintin.services;
 
+import tintin.dev.user.StudentDTO;
+import tintin.services.exceptions.StudentNotFoundException;
+
 public interface StudentService {
+	
+	public StudentDTO getStudent(Long idStudent) throws StudentNotFoundException;
+	
+	public void getStudentRegister();
 	
 }
