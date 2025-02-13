bbe0544cdf11cbb816b9809b77850eae5c20dcbb
Sanyo2704 <alvarotormento2704@gmail.com>
2025-02-13 17:57:15 +0100

    13/02/2025

--- TinTin_Project/src/main/java/tintin/services/StudentService.java
+++ TinTin_Project/src/main/java/tintin/services/StudentService.java
@@ -1,12 +1,20 @@
 package tintin.services;
 
+import java.time.LocalDate;
+
 import tintin.dto.StudentDto;
 import tintin.model.FCTRegister;
+import tintin.services.exceptions.DuplicateRegisterException;
+import tintin.services.exceptions.RegisterNotFoundException;
 import tintin.services.exceptions.StudentNotFoundException;
 
 public interface StudentService {
 	
 	public StudentDto getStudent(Long idStudent) throws StudentNotFoundException;
 	
-	public FCTRegister getRegisters() throws StudentNotFoundException;
+	public FCTRegister getRegisters(String filter, LocalDate since, LocalDate till) throws StudentNotFoundException;
+	
+	public void deleteRegister(Long idFCTRegister) throws RegisterNotFoundException;
+	
+	public FCTRegister createRegister(FCTRegister register) throws DuplicateRegisterException;
 }
