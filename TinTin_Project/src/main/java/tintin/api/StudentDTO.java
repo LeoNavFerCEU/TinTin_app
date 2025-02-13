260714214db749db634da318e01870ba9c73481b
LeoNavFerCEU <lnavarro8494@ceuandalucia.es>
2025-02-13 17:03:17 +0100

    Final commit

--- TinTin_Project/src/main/java/tintin/api/StudentDTO.java
+++
@@ -1,6 +0,0 @@
-package tintin.api;
-
-
-public class StudentDTO {
-
-}
--- TinTin_Project/src/main/java/tintin/api/UserApiService.java
+++ TinTin_Project/src/main/java/tintin/api/UserApiService.java
@@ -1,5 +1,6 @@
 package tintin.api;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -8,7 +9,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import tintin.model.User;
 import tintin.services.UserService;
+import tintin.services.UserUnauthorizedException;
+import tintin.services.exceptions.NotActiveUserException;
+import tintin.services.exceptions.StudentNotFoundException;
+import tintin.services.exceptions.UserNotFoundException;
 
 
 @RestController
@@ -19,8 +25,9 @@ public class UserApiService {
 	private UserService userService;
 	
 	@GetMapping("login")
-	public StudentDTO login(@RequestParam String username, @RequestParam String pswd) {
-		//User user = userService.login(username, pswd);
+	public StudentDTO login(@RequestParam String username, @RequestParam String pswd) throws UserNotFoundException, StudentNotFoundException, NotActiveUserException, UserUnauthorizedException {
+		User user = userService.login(username, pswd);
+		ModelMapper mapper = new ModelMapper();
 		
 		return null; //new String();
 	}
