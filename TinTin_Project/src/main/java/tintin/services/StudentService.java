fedb1547b594324cd1fbc708a1a35c683e00e52d
jfloress04 <jafd2380@gmail.com>
2025-02-13 17:21:39 +0100

    getRegisters

--- TinTin_Project/src/main/java/tintin/services/StudentService.java
+++ TinTin_Project/src/main/java/tintin/services/StudentService.java
@@ -1,5 +1,8 @@
 package tintin.services;
 
+import tintin.model.FCTRegister;
+import tintin.model.Student;
+
 public interface StudentService {
-	
+	public FCTRegister getRegisters(Student student);
 }
