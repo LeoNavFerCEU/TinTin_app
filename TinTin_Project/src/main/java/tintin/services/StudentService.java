fa35b3a33d3f35d80b9f6a5d1df1c6576dbe7b00
Sanyo2704 <alvarotormento2704@gmail.com>
2025-02-13 17:41:25 +0100

    StudentService

--- TinTin_Project/src/main/java/tintin/services/StudentService.java
+++ TinTin_Project/src/main/java/tintin/services/StudentService.java
@@ -2,12 +2,11 @@ package tintin.services;
 
 import tintin.dto.StudentDto;
 import tintin.model.FCTRegister;
-import tintin.model.Student;
 import tintin.services.exceptions.StudentNotFoundException;
 
 public interface StudentService {
 	
 	public StudentDto getStudent(Long idStudent) throws StudentNotFoundException;
 	
-	public FCTRegister getRegisters(Student student);
+	public FCTRegister getRegisters() throws StudentNotFoundException;
 }
