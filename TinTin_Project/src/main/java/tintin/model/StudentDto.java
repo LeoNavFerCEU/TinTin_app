f84bd401233a1586ce58dfe7830ddd4f52633a89
jfloress04 <jafd2380@gmail.com>
2025-02-13 17:29:19 +0100

    StudentDto implementao

---
+++ TinTin_Project/src/main/java/tintin/model/StudentDto.java
@@ -0,0 +1,16 @@
+package tintin.model;
+
+import lombok.Data;
+
+@Data
+public class StudentDto {
+	private String fullName;
+	private String course;
+	private String internshipPeriod;
+	private String courseYear;
+	private String tutorName;
+	private String companyName;
+	private Integer hoursTotal;
+	private Integer hoursWorked;
+	private Integer hoursLeft;
+}
