943d5040f1a37e268cf7cd34f5180344df8746e2
esanchis5721 <kiikeesp@gmail.com>
2025-02-18 17:19:16 +0100

    Finalización de estilos Records.css, AddRecord.css, ChangePass.css

--- FrontEnd/FrontEnd/src/main/java/app/gui/AddRecordController.java
+++ FrontEnd/FrontEnd/src/main/java/app/gui/AddRecordController.java
@@ -3,8 +3,13 @@ package app.gui;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.BorderPane;
 
-public class AddRecordController {
+public class AddRecordController extends AppController{
 
     @FXML
     private Button btnCancel;
@@ -13,13 +18,34 @@ public class AddRecordController {
     private Button btnRegister;
 
     @FXML
-    void cancel(ActionEvent event) {
+    private ComboBox<?> cbDates; // Asegúrate de tipar correctamente el ComboBox si es necesario
 
+    @FXML
+    private Spinner<Double> spHours; // Cambia Spinner<?> a Spinner<Double>
+
+    @FXML
+    private TextArea taDetail;
+
+    @FXML
+    public void initialize() {
+        // Configurar el SpinnerValueFactory para valores double
+        SpinnerValueFactory.DoubleSpinnerValueFactory valueFactory =
+                new SpinnerValueFactory.DoubleSpinnerValueFactory(0.0, 100.0, 0.0, 0.5);
+
+        // Asignar el factory al Spinner
+        spHours.setValueFactory(valueFactory);
+    }
+
+    @FXML
+    void cancel(ActionEvent event) {
+    	BorderPane mainPane = (BorderPane) getParam("Pantalla Principal");
+    	mainPane.setCenter(loadScene(FXML_RECORDS));
+        
     }
 
     @FXML
     void register(ActionEvent event) {
-
+    	BorderPane mainPane = (BorderPane) getParam("Pantalla Principal");
+    	mainPane.setCenter(loadScene(FXML_RECORDS));
     }
-
 }
