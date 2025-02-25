fc10b675092436a151b1f42e2eeed758f19a9763
Sanyo <alvarotormento2704@gmail.com>
2025-02-25 03:28:14 +0100

    Modificacion del service, queda modificar el metodo de crear registro y modificar el getStudent para que muestre los datos necesarios

--- TinTin_Project/src/main/java/tintin/repositories/FCTRegisterRepository.java
+++ TinTin_Project/src/main/java/tintin/repositories/FCTRegisterRepository.java
@@ -1,22 +1,19 @@
 package tintin.repositories;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import tintin.model.Dates;
 import tintin.model.FCTRegister;
 import tintin.model.Student;
 @Repository
 public interface FCTRegisterRepository extends JpaRepository<FCTRegister, Long>{
 	public List<FCTRegister> findByAssociatedStudent(Student student);
 	
-	public FCTRegister findOneByAssociatedDate(Dates date);
+	public FCTRegister findOneByAssociatedDate_Date(LocalDate date);
 	
 	public List<FCTRegister> findAllByAssociatedStudent(Long idstudent);
-	
-	public List<FCTRegister> findAllByNotAssociatedStudent(Long idstudent);
-	
-	public List<FCTRegister> findAllByAssociatedDate(List<Dates> dates);
+
 }
